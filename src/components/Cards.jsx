991190131943919b6caaf8045f53f6ccfd59e1f6
sofia victoria franco <sofiavictoriafranco@hotmail.com>
2023-02-22 11:39:09 -0300

    first commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,46 @@
+import Card from './Card';
+
+export default function Cards(props) {
+   const { characters } = props;
+   return (
+      
+      <div>
+         {
+
+        characters.map(({name, species, gender, image }) => { 
+
+         
+
+            return <Card
+
+            name= {name}
+            species={species}
+            gender={gender}
+            image={image}
+            onClose={() => window.alert('Emulamos que se cierra la card') }
+         
+            
+            
+            
+            />
+
+         
+
+
+
+
+         
+
+
+
+        } )
+        
+        }
+
+
+         
+
+      </div>
+
+   )
+}
